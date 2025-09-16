9c4b28fab88f7c696dc3d14acf6ad485879813cc
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2025-09-16 21:46:14 +0200

    trying to fix "A Serverless Function has exceeded the unzipped maximum size..""

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 const nextConfig = {
     webpack: (config) => {        
-        serverExternalPackages: ['sharp', 'onnxruntime-node'],
+        // serverExternalPackages: ['sharp', 'onnxruntime-node'],
         config.resolve.alias = {
           ...config.resolve.alias,
           "sharp$": false,
-          "onnxruntime-node$": false,
+          // "onnxruntime-node$": false,
           "onnxruntime-web": path.join(__dirname, 'node_modules/@huggingface/transformers/node_modules/onnxruntime-web/dist/ort.all.bundle.min.mjs'),
         }
 
