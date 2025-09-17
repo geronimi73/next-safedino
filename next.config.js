e28897ab0e08e759bce99c46e1f3a4c1dd882136
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2025-09-17 05:51:26 +0200

    seems like outputFileTracingExcludes did the trick

--- next.config.js
+++ next.config.js
@@ -20,8 +20,6 @@ const nextConfig = {
     webpack: (config) => {        
         config.resolve.alias = {
           ...config.resolve.alias,
-          // "sharp$": false,
-          // "onnxruntime-node$": false,
           "onnxruntime-web": path.join(__dirname, 'node_modules/@huggingface/transformers/node_modules/onnxruntime-web/dist/ort.all.bundle.min.mjs'),
         }
 
